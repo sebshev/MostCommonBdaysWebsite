56611c03ca1be00603c3da47dd70d7ecb6518a15
Sebastian Shevlin <seb112697@gmail.com>
2021-08-09 00:13:14 -0700

    Created Birthday class

---
+++ MostCommonBdaysWebsite/Models/Birthday.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MostCommonBdaysWebsite.Models
+{
+    public class Birthday
+    {
+        public string BirthDate { get; set; }
+
+        public int Uniqueness { get; set; }
+    }
+}
