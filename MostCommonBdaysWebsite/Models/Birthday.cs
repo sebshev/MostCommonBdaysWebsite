7fb7e48151c24097e51edad78c127d795a0bc236
Sebastian Shevlin <seb112697@gmail.com>
2021-08-11 12:29:17 -0700

    Added date time annotation

--- MostCommonBdaysWebsite/Models/Birthday.cs
+++ MostCommonBdaysWebsite/Models/Birthday.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,9 @@ namespace MostCommonBdaysWebsite.Models
 {
     public class Birthday
     {
-        public string BirthDate { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime BirthDate { get; set; }
 
         public int Uniqueness { get; set; }
     }
